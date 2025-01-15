f346558ad2cf7ed4794fc2531cafaf436004be10
Michael <mppattison@shaw.ca>
2025-01-14 18:51:32 -0700

    new1

--- ListCity1/app/src/main/java/com/example/listcity1/MainActivity.java
+++ ListCity1/app/src/main/java/com/example/listcity1/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.listcity1;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.ListView;
 
 import androidx.activity.EdgeToEdge;
@@ -23,20 +26,38 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
-        //required lines
+
         super.onCreate(savedInstanceState);
-        //EdgeToEdge.enable(this);
+        EdgeToEdge.enable(this);
 
-
-        // optional
         setContentView(R.layout.activity_main);
 
 
         cityList = findViewById(R.id.city_list);
+
+        Button button = findViewById(R.id.button);
+
         String[] cities = {"Edmonton", "Paris", "London"};
         dataList = new ArrayList<>();
         dataList.addAll(Arrays.asList(cities));
         cityAdapter = new ArrayAdapter<>(this, R.layout.content, dataList);
         cityList.setAdapter(cityAdapter);
+
+        cityList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+
+            }
+        });
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                dataList.add("Ottowa");
+                cityAdapter.notifyDataSetChanged();
+            }
+        });
+
+
     }
 }
